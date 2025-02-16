99d3b90d4d384ce1b44406484a7217325f15e37f
Pablo Sgreccia <pablosgreccia@gmail.com>
2025-02-15 21:35:50 -0300

    ADD: user repository tests

--- test/user/user.repository.spec.ts
+++ test/user/user.repository.spec.ts
@@ -2,46 +2,83 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
 import { UserRepository } from 'src/user/user.repository';
-import { Repository } from 'typeorm';
 import { mockRegularUser } from '../mocks/user.mock';
+import { Repository } from 'typeorm';
 
 describe('User Repository', () => {
 	let userRepository: UserRepository;
-	let mockEntityRepository: Partial<Repository<User>>;
+	let repositoryMock: Repository<User>;
 
 	beforeEach(async () => {
-		mockEntityRepository = {
-			findOneBy: jest.fn(),
-		};
-
 		const module: TestingModule = await Test.createTestingModule({
 			providers: [
 				UserRepository,
 				{
 					provide: getRepositoryToken(User),
-					useValue: mockEntityRepository,
+					useValue: {
+						create: jest.fn(),
+						save: jest.fn(),
+						findOneBy: jest.fn(),
+					},
 				},
 			],
 		}).compile();
 
 		userRepository = module.get<UserRepository>(UserRepository);
+		repositoryMock = module.get<Repository<User>>(getRepositoryToken(User));
 	});
 
-	it('should return a user if found', async () => {
-		mockEntityRepository.findOneBy = jest.fn().mockResolvedValue(mockRegularUser);
+	describe('createUserInstance', () => {
+		it('should create a user instance', async () => {
+			const userToInstance = { username: mockRegularUser.username, password: mockRegularUser.password, isAdmin: mockRegularUser.isAdmin };
+			repositoryMock.create = jest.fn().mockReturnValue(mockRegularUser);
 
-		const result = await userRepository.getOneByUsername(mockRegularUser.username);
+			const result = await userRepository.createUserInstance(userToInstance);
 
-		expect(result).toEqual(mockRegularUser);
-		expect(mockEntityRepository.findOneBy).toHaveBeenCalledWith({ username: mockRegularUser.username });
+			expect(repositoryMock.create).toHaveBeenCalledWith(userToInstance);
+			expect(result).toEqual(mockRegularUser);
+		});
 	});
 
-	it('should return null if user is not found', async () => {
-		mockEntityRepository.findOneBy = jest.fn().mockResolvedValue(null);
+	describe('insert', () => {
+		it('should insert a new user', async () => {
+			repositoryMock.save = jest.fn().mockResolvedValue(mockRegularUser);
+			const result = await userRepository.insert(mockRegularUser);
 
-		const result = await userRepository.getOneByUsername('unknown username');
+			expect(repositoryMock.save).toHaveBeenCalledWith(mockRegularUser);
+			expect(result).toEqual(mockRegularUser);
+		});
+	});
 
-		expect(result).toBeNull();
-		expect(mockEntityRepository.findOneBy).toHaveBeenCalledWith({ username: 'unknown username' });
+	describe('get one by username', () => {
+		it('should return a user by username', async () => {
+			repositoryMock.findOneBy = jest.fn().mockResolvedValue(mockRegularUser);
+			const result = await userRepository.getOneByUsername(mockRegularUser.username);
+			expect(repositoryMock.findOneBy).toHaveBeenCalledWith({ username: mockRegularUser.username });
+			expect(result).toEqual(mockRegularUser);
+		});
+
+		it('should return null if user is not found', async () => {
+			repositoryMock.findOneBy = jest.fn().mockResolvedValue(null);
+			const result = await userRepository.getOneByUsername('unknownUser');
+			expect(repositoryMock.findOneBy).toHaveBeenCalledWith({ username: 'unknownUser' });
+			expect(result).toBeNull();
+		});
+	});
+
+	describe('get one by id', () => {
+		it('should return a user by id', async () => {
+			repositoryMock.findOneBy = jest.fn().mockResolvedValue(mockRegularUser);
+			const result = await userRepository.getOneById(1);
+			expect(repositoryMock.findOneBy).toHaveBeenCalledWith({ id: mockRegularUser.id });
+			expect(result).toEqual(mockRegularUser);
+		});
+
+		it('should return null if user is not found', async () => {
+			repositoryMock.findOneBy = jest.fn().mockResolvedValue(null);
+			const result = await userRepository.getOneById(999);
+			expect(repositoryMock.findOneBy).toHaveBeenCalledWith({ id: 999 });
+			expect(result).toBeNull();
+		});
 	});
 });
